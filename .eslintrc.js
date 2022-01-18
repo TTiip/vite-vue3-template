97cf0171605427558c3c549b80597b942e575cd9
TTiip <2711430287@qq.com>
2022-01-18 10:49:30 +0800

    script 标签 缩进两格空格修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,21 @@ module.exports = {
     // 子组件修改父组件的值时报错提示
     'vue/no-mutating-props': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // script标签缩进设置
+    "vue/script-indent": ["error", 2, {
+      "baseIndent": 1,
+      "switchCase": 0,
+      "ignores": []
+    }]
+
+  },
+  overrides: [
+    {
+      "files": ["*.vue"],
+      "rules": {
+        "indent": "off"
+      }
+    }
+  ]
 }
