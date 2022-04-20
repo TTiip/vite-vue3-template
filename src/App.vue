0cb8d99fde3d592bdbcc83b7db40740e9ec039e8
TTiip <2711430287@qq.com>
2022-04-20 12:24:07 +0800

    feat: APP 页面添加路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <Layout>
     <PiniaLogo />
+    <div style="text-align: center;margin-top: 20px;" class="aa">
+      <router-link class="btn" to="/Bar">
+        Bar
+      </router-link>
+      |
+      <router-link class="btn" to="/Foo">
+        Foo
+      </router-link>
+    </div>
   </Layout>
 </template>
 
@@ -8,12 +17,22 @@
   import Layout from '@/components/Layout/index.vue'
   import PiniaLogo from '@/components/PiniaLogo.vue'
   let a = 1
-  console.log()
-  debugger
+  console.log(a)
+// debugger
 </script>
 
 <style scoped lang="less">
-body {
-  margin: 0 !important;
-}
+  body {
+    margin: 0 !important;
+  }
+
+  .btn {
+    display: inline-block;
+    border-radius: 4px;
+    padding: 2px 16px;
+    text-decoration: none;
+    border: 1px solid #83e695;
+    background: #83e695;
+    color: #fff;
+  }
 </style>
