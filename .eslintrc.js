3b314fdc5e66c26a4cd879e0da527142b5b4f468
TTiip <31298116+TTiip@users.noreply.github.com>
2022-05-07 17:06:53 +0800

    chore: 修改 `.eslint.js` 配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,37 +28,39 @@ module.exports = {
     // 函数参数 不使用换行
     'function-paren-newline': ['error', 'never'],
     // 函数左括号前一定要加 空格
+    '@typescript-eslint/space-before-function-paren': ['error', 'always'],
+    // 函数左括号前一定要加 空格
     'space-before-function-paren': ['error', 'always'],
     // 当仅有一个判断的时候，必须使用 {} 包裹
     'curly': ['error', 'all'],
     // 花括号是不是换行
-    'object-curly-newline': ['error', {
-      ObjectExpression: {
-        // 多个换行
-        multiline: true,
-        // 超过或等于这个数量就换行
-        minProperties: 1,
-        // 如果对象中属性注释或不存在属性换行不报错。
-        consistent: true
-      },
-      ObjectPattern: {
-        multiline: true,
-        minProperties: 1
-      },
-      ImportDeclaration: {
-        multiline: true,
-        minProperties: 2
-      },
-      ExportDeclaration: {
-        multiline: true,
-        minProperties: 2
-      }
-    }],
+    // 'object-curly-newline': ['error', {
+    //   ObjectExpression: {
+    //     // 多个换行
+    //     multiline: true,
+    //     // 超过或等于这个数量就换行
+    //     minProperties: 1,
+    //     // 如果对象中属性注释或不存在属性换行不报错。
+    //     consistent: true
+    //   },
+    //   ObjectPattern: {
+    //     multiline: true,
+    //     minProperties: 1
+    //   },
+    //   ImportDeclaration: {
+    //     multiline: true,
+    //     minProperties: 2
+    //   },
+    //   ExportDeclaration: {
+    //     multiline: true,
+    //     minProperties: 2
+    //   }
+    // }],
     // 强制将对象的属性放在不同的行上
-    'object-property-newline': ['error', {
-      // 对象属性放在同一行(true 不换行) 但是不知道为什么不生效。。。
-      allowAllPropertiesOnSameLine: false
-    }],
+    // 'object-property-newline': ['error', {
+    //   // 对象属性放在同一行(true 不换行) 但是不知道为什么不生效。。。
+    //   allowAllPropertiesOnSameLine: false
+    // }],
     // 不允许使用tab缩进
     'no-tabs': 'off',
     // 不允许使用 console
@@ -68,7 +70,7 @@ module.exports = {
     // 禁止使用特定的语法
     'no-restricted-syntax': 'off',
     // typescript 关闭逗号检测
-    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'never'],
     // 关闭缩进检测
     '@typescript-eslint/indent': 'off',
     // {} 括号和 else 换行风格。
